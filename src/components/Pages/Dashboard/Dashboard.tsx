bb54bd5801287693661d84b34c1f209bf993af4c
Somtochukwu Nnaji <lyon4197@gmail.com>
2023-07-03 00:09:32 +0100

    Fixed my prettier error

--- src/components/Pages/Dashboard/Dashboard.tsx
+++ src/components/Pages/Dashboard/Dashboard.tsx
@@ -1,20 +1,20 @@
 import { MaxCard } from "../../Global/Card/MaxCard/MaxCard";
 import { BetManagement } from "./BetManagement/BetManagement";
-import { UserStatusCard } from "./UserStatusCard/UserStatusCard";
 import { UsersInfo } from "./UsersInfo/UsersInfo";
+import { UserStatusCard } from "./UserStatusCard/UserStatusCard";
 
 export const Dashboard = () => {
-	return (
-		<MaxCard className="w-full">
-			<div className="flex flex-wrap lg:flex-nowrap  lg:space-x-6">
-				<div className="w-full space-y-3">
-					<UsersInfo/>
-					<BetManagement/>
-				</div>
-				<div className="lg:w-1/2">
-					<UserStatusCard />
-				</div>
-			</div>
-		</MaxCard>
-	);
+  return (
+    <MaxCard className="w-full">
+      <div className="flex flex-wrap lg:flex-nowrap  lg:space-x-6">
+        <div className="w-full space-y-3">
+          <UsersInfo />
+          <BetManagement />
+        </div>
+        <div className="lg:w-1/2">
+          <UserStatusCard />
+        </div>
+      </div>
+    </MaxCard>
+  );
 };
