a2089e4bf32d04546aa9619ed92bc0171012cafa
N Cee Dee <nnajidavid58@gmail.com>
2023-07-03 02:19:33 +0100

    added the routerName to dashboard

--- src/components/Pages/Dashboard/Dashboard.tsx
+++ src/components/Pages/Dashboard/Dashboard.tsx
@@ -2,10 +2,12 @@ import { MaxCard } from "../../Global/Card/MaxCard/MaxCard";
 import { UsersInfo } from "./UsersInfo/UsersInfo";
 import { BetManagement } from "./BetManagement/BetManagement";
 import { UserStatusCard } from "./UserStatusCard/UserStatusCard";
+import { Header } from "../../Layouts/Header/Header";
 
 export const Dashboard = () => {
 	return (
 		<MaxCard className="w-full">
+			<Header/>
 			<div className="flex flex-wrap lg:flex-nowrap  lg:space-x-6">
 				<div className="w-full space-y-3">
 					<UsersInfo/>
