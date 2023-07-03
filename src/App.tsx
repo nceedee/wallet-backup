f317c35e487a6f925987323dc8b7bdae60734c26
Somtochukwu Nnaji <lyon4197@gmail.com>
2023-07-03 16:40:43 +0100

    Fixed and added bets table

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { SideBar } from "./components/Layouts/SideBar/SideBar";
 import { Dashboard } from "./components/Pages/Dashboard/Dashboard";
-import { Users } from "./components/Pages/Users/Users"; 
+import { Users } from "./components/Pages/Users/Users";
 
 const App = () => {
   return (
-    <div className="flex h-[100vh] w-full bg-primary font-inter tracking-wide">
+    <div className="flex h-[100vh] w-full  bg-primary font-inter tracking-wide">
       <div>
         <SideBar />
       </div>
-      <div className="w-full ">
+      <div className="w-full overflow-y-scroll">
         <Routes>
           <Route path="/" element={<Dashboard />}></Route>
           <Route path="/users" element={<Users />}></Route>
--- src/components/Pages/Dashboard/Dashboard.tsx
+++ src/components/Pages/Dashboard/Dashboard.tsx
@@ -6,7 +6,7 @@ import { UserStatusCard } from "./UserStatusCard/UserStatusCard";
 
 export const Dashboard = () => {
   return (
-    <MaxCard className="w-full">
+    <MaxCard className=" w-full ">
       <Header />
       <div className="flex flex-wrap lg:flex-nowrap  lg:space-x-9">
         <div className="w-full space-y-3">
