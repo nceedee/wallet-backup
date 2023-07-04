802438eca568da14162e1b707aea889ec580993c
N Cee Dee <nnajidavid58@gmail.com>
2023-07-04 12:15:16 +0100

    structured the header to be flexible

--- src/components/Pages/Users/Users.tsx
+++ src/components/Pages/Users/Users.tsx
@@ -1,11 +1,11 @@
 import { MaxCard } from "../../Global/Card/MaxCard/MaxCard";
-import { RouterName } from "../../Layouts/Header/RouterName";
+import { Header } from "../../Layouts/Header/Header";
 import { UsersInfoTable } from "./UsersInfoTable";
 
 export const Users = () => {
   return (
     <MaxCard className=" bg-primary text-xl font-semibold text-gray-900 ">
-      <RouterName routerName="Users" />
+      <Header routerName="Users" />
       <UsersInfoTable />
     </MaxCard>
   );
--- src/components/Pages/Users/UsersInfoTable.tsx
+++ src/components/Pages/Users/UsersInfoTable.tsx
@@ -2,13 +2,14 @@ import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "AllUsers", headerName: "All Users", width: 120 },
+  { field: "active", headerName: "Active Users", width: 120 },
   { field: "lastName", headerName: "Last name", width: 130 },
   {
     field: "age",
     headerName: "Age",
     type: "number",
-    width: 90,
+    width: 100,
   },
   {
     field: "fullName",
@@ -21,20 +22,20 @@ const columns: GridColDef[] = [
 ];
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 1, active: false, AllUsers: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, active: false, AllUsers: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, active: false, AllUsers: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, active: false, AllUsers: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, active: false, AllUsers: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, active: false, AllUsers: "Melisandre", firstName: null, age: 150 },
+  { id: 7, active: false, AllUsers: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, active: false, AllUsers: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, active: false, AllUsers: "Roxie", firstName: "Harvey", age: 65 },
 ];
 
 export const UsersInfoTable = () => {
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div style={{ height: "auto", width: "100%", background: "white" }}>
       <DataGrid
         rows={rows}
         columns={columns}
