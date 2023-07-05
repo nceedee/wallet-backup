72203db3085c00ac3724a0ed465854fd4a9393d2
Somtochukwu Nnaji <lyon4197@gmail.com>
2023-07-05 16:12:23 +0100

    Changed the table

--- src/components/Pages/Dashboard/BetManagement/BetManagement.tsx
+++ src/components/Pages/Dashboard/BetManagement/BetManagement.tsx
@@ -1,16 +1,15 @@
-import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
-import TableContainer from "@mui/material/TableContainer";
+import { Card } from "../../../Global/Card/Card";
 import { TBody } from "./Tbody/TBody";
 import { THead } from "./THead/THead";
 
 export function BetManagement() {
   return (
-    <TableContainer component={Paper}>
+    <Card className="p-0 py-2">
       <Table aria-label="collapsible table">
         <THead />
         <TBody />
       </Table>
-    </TableContainer>
+    </Card>
   );
 }
