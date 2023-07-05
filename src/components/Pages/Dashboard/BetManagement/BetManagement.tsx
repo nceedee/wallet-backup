192d91646423366c051f70125e57f98e5039f570
Somtochukwu Nnaji <lyon4197@gmail.com>
2023-07-05 22:34:51 +0100

    Changed the content of the table header

--- src/components/Pages/Dashboard/BetManagement/BetManagement.tsx
+++ src/components/Pages/Dashboard/BetManagement/BetManagement.tsx
@@ -1,11 +1,13 @@
 import Table from "@mui/material/Table";
 import { Card } from "../../../Global/Card/Card";
+import { TextWrap } from "../../../Global/TextWrap/TextWrap";
 import { TBody } from "./Tbody/TBody";
 import { THead } from "./THead/THead";
 
 export function BetManagement() {
   return (
     <Card className="p-0 py-2">
+      <TextWrap className="text-sm capitalize ">football matches</TextWrap>
       <Table aria-label="collapsible table">
         <THead />
         <TBody />
--- src/components/Pages/Dashboard/BetManagement/THead/THead.tsx
+++ src/components/Pages/Dashboard/BetManagement/THead/THead.tsx
@@ -1,15 +1,27 @@
 import { TableCell, TableHead, TableRow } from "@mui/material";
 
 export const THead = () => {
+  const texts = ["", "HOME team - AWAY team", "1", "x", "2", "Stadium"];
   return (
     <TableHead>
       <TableRow>
+        {texts.map((text, i) => {
+          const right = i > 1 ? "right" : "left";
+
+          return (
+            <TableCell key={i} align={`${right}`}>
+              {text}
+            </TableCell>
+          );
+        })}
+        {/*  
         <TableCell />
-        <TableCell>Dessert (100g serving)</TableCell>
-        <TableCell align="right">Calories</TableCell>
-        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+        <TableCell align="right">HOME team - AWAY team</TableCell>
+        <TableCell align="right">1</TableCell>
+        <TableCell align="right">x</TableCell>
+        <TableCell align="right">2</TableCell>
+        <TableCell align="right">Stadium</TableCell>
+      */}
       </TableRow>
     </TableHead>
   );
